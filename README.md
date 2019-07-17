c84ef2fff18d1bdf45f8a461da8841aae119db86
hieunguyenbbr1501 <43947312+hieunguyenbbr1501@users.noreply.github.com>
2019-07-17 10:57:35 +0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Algo-php
+Yêu cầu:
+- Viết class QuestionList để lưu toàn bộ các câu hỏi trong file question.md
+- Viết class Question để lưu từng câu hỏi
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 require('QuestionList.php');
 $a = new QuestionList;
 $a->parse();
-echo '<pre>'; print_r($a); echo '</pre>';
+//echo '<pre>'; print_r($a); echo '</pre>';
+echo $a->questionList[2]->answer;
